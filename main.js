029e96574537d216fa39b2347f26441f540e4c1e
Alejandro Gaspar <caxpper@hotmail.com>
2018-02-12 09:43:59 -0800

    Memory Match version1.0 - Alejandro Gaspar

--- main.js
+++ main.js
@@ -14,42 +14,87 @@ var frontCards = ["bobble","bobble2","bomb","bomb2","dk","dk2",
     "pacman","pacman2","pole","pole2","sf","sf2",
     "galaga","galaga2","tetris","tetris2","zelda","zelda2"];
 
-var frontCards = [
-    {
-        "count": 1,
-        "class": "front bobble",
-        "src": "image/bobble.png",
-        "src_3d": "image/bobble2.png",
-        "on": {
-            "click": "card_clicked "
-        }
-    },
-    {
-
-    }
-]
-
+var sounds = {};
 /***********************************************************************************/
 $(document).ready(initApp);
 
-
-
+/**
+ *
+ */
 function initApp(){
-    create_cards();
-    $('.reset').click(function () {
-        games_played++;
-        reset_stats();
-        $('.card').removeClass("hidden");
-        $('.message').addClass("hidden");
-        $('.front').removeClass("animation_fadeToTransparent");
-        $('.front').removeClass("animation_transparentToFade");
-    });
+    initSounds();//Iniciate all sounds and store in a global var
+    create_cards(shuffle(frontCards));//create all cards from array
+    $('.reset_button').click(reset_game);
     display_stats();
 }
 
-function create_cards(){
-    var game_area = $('.gameArea');
+/**
+ *
+ */
+function initSounds() {
+    var bobble_sound = new Audio();
+    bobble_sound.src= "sound/bobble.mp3";
+    sounds["bobble"] = bobble_sound;
 
+    var sf_sound = new Audio();
+    sf_sound.src= "sound/sf.mp3";
+    sounds["sf"] = sf_sound;
+
+    var bomb_sound = new Audio();
+    bomb_sound.src= "sound/bomb.mp3";
+    sounds["bomb"] = bomb_sound;
+
+    var dk_sound = new Audio();
+    dk_sound.src= "sound/dk.wav";
+    sounds["dk"] = dk_sound;
+
+    var pacman_sound = new Audio();
+    pacman_sound.src= "sound/pacman.wav";
+    sounds["pacman"] = pacman_sound;
+
+    var pole_sound = new Audio();
+    pole_sound.src= "sound/pole.mp3";
+    sounds["pole"] = pole_sound;
+
+    var galaga_sound = new Audio();
+    galaga_sound.src= "sound/galaga.mp3";
+    sounds["galaga"] = galaga_sound;
+
+    var tetris_sound = new Audio();
+    tetris_sound.src= "sound/tetris.mp3";
+    sounds["tetris"] = tetris_sound;
+
+    var zelda_sound = new Audio();
+    zelda_sound.src= "sound/zelda.mp3";
+    sounds["zelda"] = zelda_sound;
+
+    var card_sound = new Audio();
+    card_sound.src= "sound/card.wav";
+    sounds["card"] = card_sound;
+}
+/**
+ *
+ */
+function reset_game() {
+    create_cards(shuffle(frontCards));
+    games_played++;
+    reset_stats();
+    $('.card').removeClass("flip");
+    $('.message').addClass("hidden");
+    $('.front').removeClass("animation_fadeToTransparent");
+    $('.front').removeClass("animation_transparentToFade");
+}
+
+/**
+ *
+ * @param frontCards
+ */
+function create_cards(frontCards){
+    var game_area = $('.gameArea');
+    game_area.empty();
+
+    var won_message = $('<div>').addClass('message hidden');//Won message. We hide at the begging
+    game_area.append(won_message);
     for(var i = 0; i < frontCards.length; i++){
 
         //create a new row
@@ -66,14 +111,23 @@ function create_cards(){
     }
 }
 
+/**
+ * Create a card with DOM creation
+ * @param front_class
+ * @returns {*|jQuery|HTMLElement}
+ */
 function create_card(front_class){
 
-    var card = $('<div>',{
+    var card_container = $('<div>',{//this container is to make the flip animation
+        class: "card_container"
+    });
+    var card = $('<div>',{//the actual card with the front and the back
         class: "card",
         click: card_clicked
     });
     var index = front_class.indexOf("2");
     var id;
+    //We create a common id for the 2 different card for the same game
     if(index !== -1){
         id = front_class.slice(0,index);
     }else{
@@ -102,47 +156,61 @@ function create_card(front_class){
     }
     card.append(front);
     card.append(back);
-    return card;
+    card_container.append(card);
+    return card_container;
 }
 
+/**
+ *
+ * @returns {boolean}
+ */
 function card_clicked(){
 
     if($(this).find(".animation_fadeToTransparent").length===0){//check the card is not flip already
         if (second_click) {//check you can't click when the second card was already click
             second_click = false;
-            $(this).addClass("hidden");
-            if (first_card_clicked === null) {//control
+            sounds["card"].play();
+            $(this).addClass("flip");
+            if (first_card_clicked === null) {//control is the first card click
                 first_card_clicked = $(this);
                 second_click = true;
                 return true;
             } else {
-                if (first_card_clicked.find(".front")[1] !== $(this).find(".front")[1]) {
+                if (first_card_clicked.find(".front")[1] !== $(this).find(".front")[1]) {//Don't click the same card twice
                     second_card_clicked = $(this);
                     attempts++;
+                    //when you click two cards from the same game
                     if (first_card_clicked.find(".front").attr("id") === second_card_clicked.find(".front").attr("id")) {
+                        //play a music from that game
+                        sounds[first_card_clicked.find(".front").attr("id")].play();
                         matches++;
                         accuracy = matches / attempts;
+                        //animation to transform from pixel to 3d
                         transformfromPixelTo3d(first_card_clicked, second_card_clicked);
                         first_card_clicked = null;
                         second_card_clicked = null;
                         display_stats();
                         second_click = true;
+                        //when you have all the matches
                         if (total_possible_matches === matches) {
-                            $('.message').text("You Won!");
-                            $('.message').removeClass("hidden");
+                            setTimeout(function () {
+                                $('.message').text("You Won!");
+                                $('.message').removeClass("hidden");
+                            }, 2000);
                         } else {
                             return true;
                         }
                     } else {
                         setTimeout(function () {
+                            //flip to back both cards
                             accuracy = matches / attempts;
-                            first_card_clicked.removeClass("hidden");
-                            second_card_clicked.removeClass("hidden");
+                            first_card_clicked.removeClass("flip");
+                            second_card_clicked.removeClass("flip");
                             first_card_clicked = null;
                             second_card_clicked = null;
                             display_stats();
                             second_click = true;
-                        }, 2000);
+                        }, 1000);
                         return true;
                     }
                 } else {
@@ -154,6 +222,9 @@ function card_clicked(){
     }
 }
 
+/**
+ *
+ */
 function display_stats(){
     $('.games-played span').text(games_played);
     $('.attempts span').text(attempts);
@@ -161,6 +232,9 @@ function display_stats(){
     $('.accuracy span').text(format_accuracy);
 }
 
+/**
+ *
+ */
 function reset_stats() {
     accuracy = 0;
     matches = 0;
@@ -186,4 +260,23 @@ function transformfromPixelTo3d(first_card_clicked,second_card_clicked) {
         first_card_clicked.find("." + first_card_clicked.find(".front").attr("id") + "3").addClass("animation_fadeToTransparent");
         first_card_clicked.find("." + first_card_clicked.find(".front").attr("id")).addClass("animation_transparentToFade");
     }
+}
+
+/**
+ * Shuffle the elements in an array
+ * @param array
+ * @returns {*}
+ */
+function shuffle (array) {
+
+    var j = 0
+    var temp = null;
+
+    for (var i = array.length - 1; i > 0; i -= 1) {
+        j = Math.floor(Math.random() * (i + 1))
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array;
 }
